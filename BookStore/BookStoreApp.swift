e84238857b0141d2dd6efd7774d192bb5ca36294
Joyce Rosario Batista <joyce.rosbat@gmail.com>
2023-01-30 18:30:28 +0000

    Initial Commit

---
+++ BookStore/BookStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookStoreApp.swift
+//  BookStore
+//
+//  Created by Joyce Rosario Batista on 30/1/23.
+//
+
+import SwiftUI
+
+@main
+struct BookStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
